5d166129c023cc31af7142d231c16f89635488a1
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-01-11 15:29:51 +0400

    add task

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import './add-item.css';
 
 export default class AddItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      label: ''
+    };
+  }
+
+  onLabelChange = e => {
+    this.setState({ label: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.ItemAdd(this.state.label);
+    this.setState({label:''})
+  };
   render() {
     return (
-      <div className='add-item'>
-        <button
-          className='btn btn-outline-primary'
-          onClick={() => this.props.ItemAdd('hello world')}
-        >
-          Add Item
-        </button>
-      </div>
+      <form className='add-item d-flex' onSubmit={this.onSubmit}>
+        <input
+          type='text'
+          className='form-control'
+          onChange={this.onLabelChange}
+          placeholder=' Add New Task'
+          value={this.state.label}
+        />
+        <button className='btn btn-outline-primary'>Add Task</button>
+      </form>
     );
   }
 }
